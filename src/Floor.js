8e344edb11267fcd208958115fe011fa26cd97ff
Abby Xu <abby.tsui2016@gmail.com>
2019-11-21 14:42:56 -0800

    toggled floor and camera

--- src/Floor.js
+++ src/Floor.js
@@ -6,13 +6,12 @@ function toggleFloor(scene, floor) {
 	let floorMaterial = new THREE.MeshLambertMaterial({ color: 0xF3FFE2 });
 	let floorMesh = new THREE.Mesh(floor, floorMaterial);
 	floorMesh.name = "floor";
+	
 
 	if (floorSwitch.checked) {
 		const selectedObject = scene.getObjectByName(floor.name);
 		scene.remove(selectedObject);
 	} else {
-		
-		
 		floorMesh.rotation.x = -90 * Math.PI / 180;
 		floorMesh.position.y = -150;
 		scene.add(floorMesh);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Geometry from "./Geometry";
 import Material from "./Material";
 import Light from "./Light";
 import Lighting from "./Lighting";
-import toggleFloor from "./Floor";
+// import toggleFloor from "./Floor";
 import toggleButton from "./toggleButton";
 
 
@@ -16,7 +16,8 @@ const maxHeight = window.innerHeight;
 renderer.setSize(maxWidth, maxHeight);
 
 // init camera
-let camera = new THREE.PerspectiveCamera(35, maxWidth / maxHeight, 300, 10000);
+let camera1 = new THREE.PerspectiveCamera(35, maxWidth / maxHeight, 300, 10000);
+let camera2 = new THREE.OrthographicCamera(-1500, 1500, 1500, -1500, 0.1, 10000);
 
 // camera.position.set(0, 0, 0);
 
@@ -24,13 +25,16 @@ let camera = new THREE.PerspectiveCamera(35, maxWidth / maxHeight, 300, 10000);
 let scene = new THREE.Scene();
 
 // add lights
-let light1 = new THREE.AmbientLight(0xffffff, 0.4);
+let light1 = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(light1);
-let light2 = new THREE.PointLight(0xffffff, 0.4);
+let light2 = new THREE.PointLight(0xffffff, 0.5); // 600
 scene.add(light2);
 
 // init floor
-let floor = new THREE.PlaneGeometry(10000, 10000, 100, 100);
+// let floor = new THREE.PlaneGeometry(10000, 10000, 100, 100);
+// let floorMaterial = new THREE.MeshLambertMaterial({ color: 0xF3FFE2 });
+// let floorMesh = new THREE.Mesh(floor, floorMaterial);
+// floorMesh.name = "floor";
 
 // first render
 fullRender();
@@ -52,7 +56,7 @@ function fullRender() {
 	// let lightArr = model.lightArr;
 	// console.log(lightArr)
 
-	console.log(`previous ShapeName: ${ model.previousShapeName }`)
+	// console.log(`previous ShapeName: ${ model.previousShapeName }`)
 	clearObjectFromScene(scene, model.previousShapeName);
 
 	// Light.controlLight(scene, lightArr);
@@ -63,9 +67,8 @@ function fullRender() {
 	// 	Lighting.removeLight(scene);
 	// }
 	
-	console.log(`shape before change: ${ Model.shapeName() }`);
+	// console.log(`shape before change: ${ Model.shapeName() }`);
 	let geometry = Geometry.setShape(Model.shapeName(), scale, detail);
-	// Update previousShapeName so we can delete correctly next time.
 	model.previousShapeName = Model.shapeName();
 
 	let material = Material.setMaterial(Model.materialName(), color);
@@ -87,23 +90,46 @@ function fullRender() {
 	mesh.name = Model.shapeName();
 	scene.add(mesh);
 
-	// add floor
-	// setFloor(scene);
-	
-	toggleFloor(scene, floor);
-
-	
-	// toggle Light;
+	// toggle Light
 	// new Lighting();
 	// Lighting.toggleLight(scene);
 
+	// toggle Floor
+	let floor = new THREE.PlaneGeometry(10000, 10000, 100, 100);
+	let floorMaterial = new THREE.MeshLambertMaterial({ color: 0xd3d3d3 });
+	let floorMesh = new THREE.Mesh(floor, floorMaterial);
+	floorMesh.name = "floor";
+	let floorSwitch = document.getElementById("floor");
+	
+	if (floorSwitch.checked) {
+		floorMesh.rotation.x = -90 * Math.PI / 180;
+		floorMesh.position.y = -150;
+		scene.add(floorMesh);
+	} else {
+		clearObjectFromScene(scene, "floor");
+	}
 
+	// add spotlight
+	// let spotLight = new THREE.SpotLight(0xffffff, 2.0, 30); // distance
+	// spotLight.name = "Spot Light";
+	// spotLight.target = mesh;
 	requestAnimationFrame(render);
+
+	// let delta = 0;
+	// function render() {
+	// 	delta += 0.01;
+	// 	camera2.lookAt(spotLight.position);
+
+	// 	camera2.position.x = Math.sin(delta) * 5000;
+	// 	camera2.position.z = Math.cos(delta) * 5000;
+	// 	renderer.render(scene, camera2);
+	// 	requestAnimationFrame(render);
+	// }
 
 	function render() {
 		mesh.rotation.x += 0.005;
 		mesh.rotation.y += 0.005;
-		renderer.render(scene, camera);
+		renderer.render(scene, camera1);
 		requestAnimationFrame(render);
 	}
 }
@@ -117,22 +143,6 @@ let selectMaterial = document.getElementById("material");
 selectMaterial.addEventListener('change', () => {
 	fullRender();
 });
-
-// let lightArr = document.getElementsByName("light");
-
-// lightArr.forEach((light) => {
-// 	light.addEventListener('change', () => {
-// 		let lights = document.getElementsByName("light");
-// 		let length = lights.length;
-// 		for (let i = 0; i < length; i++) {
-// 			let light = lights[i];
-// 			Model.getModel().lightArr.set(light.id, light.checked);
-// 		}
-// 		fullRender();
-// 	});
-// })
-
-
 
 
 let scaleSlider = document.getElementById("scale");
@@ -153,12 +163,12 @@ colorSlider.oninput = function () {
 	fullRender();
 }
 
-let checkboxFloor = document.getElementById("floor");
-checkboxFloor.addEventListener("change", () => {
+let floorSwitch = document.getElementById("floor");
+floorSwitch.addEventListener("change", () => {
 	fullRender();
 })
 
-let toggleLight = document.getElementById("Hemisphere Light");
+let toggleLight = document.getElementById("Hemisphere-Light");
 toggleLight.addEventListener("click", () => {
 	if (toggleLight.checked) {
 		// toggleLight.checked = true;
@@ -171,6 +181,20 @@ toggleLight.addEventListener("click", () => {
 		// fullRender();
 	}
 })
+
+// let lightArr = document.getElementsByName("light");
+
+// lightArr.forEach((light) => {
+// 	light.addEventListener('change', () => {
+// 		let lights = document.getElementsByName("light");
+// 		let length = lights.length;
+// 		for (let i = 0; i < length; i++) {
+// 			let light = lights[i];
+// 			Model.getModel().lightArr.set(light.id, light.checked);
+// 		}
+// 		fullRender();
+// 	});
+// })
 
 
 // const changeValue = (field) => {
