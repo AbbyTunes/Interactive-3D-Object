f455a97be828fe79e83feef6345fdb78c0daf653
Abby Xu <abby.tsui2016@gmail.com>
2019-11-20 17:43:02 -0800

    add sliders

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ scene.add(light1);
 var light2 = new THREE.PointLight(0xffffff, 0.5);
 scene.add(light2);
 
+
+
 // init geometry
 var selectShape = document.getElementById("shape");
 let shapeName = "cube";
@@ -36,9 +38,28 @@ let materialIns = new Material();
 let material = materialIns.setMaterial(materialName)
 
 // first render
-fullRender(geometry, material, shapeName, 0, 0);
+fullRender();
 
-function fullRender(geometry, material, shapeName, offsetX, offsetY ) {
+function clearObjectFromScene(name) {
+	const selectedObject = scene.getObjectByName(name);
+	scene.remove(selectedObject);
+}
+
+function fullRender(scale=100, detail=20, color=8000, offsetX=0, offsetY=0) {
+
+	clearObjectFromScene(shapeName);
+
+	console.log(`shape before change: ${shapeName}`);
+	shapeName = selectShape.options[selectShape.selectedIndex].value;
+	console.log(`detail after change: ${detail}`)
+	geometry = geometryIns.setShape(shapeName, scale, detail);
+	// console.log(`shape after change: ${shapeName}`)
+	console.log(`scale after change: ${scale}`);
+
+	materialName = selectMaterial.options[selectMaterial.selectedIndex].value;
+	material = materialIns.setMaterial(materialName);
+	material.needsUpdate = true;
+	// console.log(`material after change: ${materialName}`)
 
 	let mesh;
 
@@ -50,6 +71,18 @@ function fullRender(geometry, material, shapeName, offsetX, offsetY ) {
 	} else {
 		mesh = new THREE.Mesh(geometry, material);
 	}
+
+	// var light3 = new THREE.DirectionalLight(0xffffff, 0.5, 100);
+	// light3.target = mesh;
+	// scene.add(light3);
+
+	// var light4 = new THREE.SpotLight(0xffffff, 2.0, 100);
+	// light4.target = mesh;
+	// scene.add(light4);
+
+	// var light5 = new THREE.HemisphereLight(0xffffff, 0x0808dd, 0.5);
+	// scene.add(light5);
+
 
 	mesh.position.set(offsetX, offsetY, -1000);
 	mesh.name = shapeName;
@@ -65,30 +98,55 @@ function fullRender(geometry, material, shapeName, offsetX, offsetY ) {
 	}
 }
 
-
 selectShape.addEventListener('change', () => {
-
-	// console.log(`shape before change: ${shapeName}`);
-	var selectedObject = scene.getObjectByName(shapeName);
-	scene.remove(selectedObject);
-
-	shapeName = selectShape.options[selectShape.selectedIndex].value;
-	geometry = geometryIns.setShape(shapeName);
-
-	// console.log(`shape after change: ${shapeName}`)
-	fullRender(geometry, material, shapeName, 0, 0);
+	fullRender();
 });
 
 selectMaterial.addEventListener('change', () => {
-
-	var selectedObject = scene.getObjectByName(shapeName);
-	scene.remove(selectedObject);
-
-	materialName = selectMaterial.options[selectMaterial.selectedIndex].value;
-	material = materialIns.setMaterial(materialName);
-	material.needsUpdate = true;
-	
-	console.log(`material after change: ${materialName}`)
-	console.log(material)
-	fullRender(geometry, material, shapeName, 0, 0);
+	fullRender();
 });
+
+var scaleSlider = document.getElementById("scale");
+scaleSlider.oninput = function () {
+	let scale = this.value;
+	fullRender(scale);
+}
+
+var detailSlider = document.getElementById("detail");
+detailSlider.oninput = function () {
+	let detail = this.value;
+	fullRender(detail);
+}
+
+
+// const changeValue = (field) => {
+// 	document.getElementById(field).oninput = function() {
+// 		let [field] = this.value;
+// 		fullRender(field);
+// 	}
+// }
+
+// update(field) {
+// 	return e => this.setState({ [field]: e.target.value });
+// }
+
+var colorSlider = document.getElementById("color");
+
+colorSlider.oninput = function () {
+	let color = parseInt(this.value);
+	fullRender(color);
+	// might be better off using this:
+	// https://www.w3schools.com/colors/colors_picker.asp
+	// fullRender({ color: parseInt(this.value) });
+}
+
+// scaleSlider.addEventListener('change', () => {
+// 	// clearObjectFromScene(shapeName);
+
+// 	shapeName = selectShape.options[selectShape.selectedIndex].value;
+// 	geometry = geometryIns.setShape(shapeName, { scale: this.value });
+
+// 	// console.log(`shape after change: ${shapeName}`)
+// 	fullRender(geometry, material, shapeName);
+// });
+
